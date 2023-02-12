155f715742fd11cf7ed02c0c5cba448ae9535494
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-02-11 23:43:04 -0300

    feat(pokedex): fetching logic

--- src/pages/Pokedex/Pokedex.js
+++ src/pages/Pokedex/Pokedex.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import getPokemons from '../../redux/actions/getPokemons';
 
 function Pokedex() {
+  const dispatch = useDispatch();
+  const { pokedex, isLoading } = useSelector((state) => state.pokemonReducer);
+
+  useEffect(() => {
+    if (!pokedex.length) {
+      dispatch(getPokemons());
+    }
+  }, []);
+
+  if (isLoading) { return <div>Loading...</div>; }
   return <div>Pokedex</div>;
 }
 
--- src/redux/reducers/pokemonReducer.js
+++ src/redux/reducers/pokemonReducer.js
@@ -1,13 +1,22 @@
-import { GET_POKEMONS } from '../actions';
+import { SET_IS_ERROR, SET_IS_LOADING, SET_POKEDEX, SET_POKEMONS } from '../actions';
 
 const INITIAL_STATE = {
+  pokedex: [],
   pokemons: [],
+  isLoading: true,
+  isError: null,
 };
 
 const pokemonReducer = (state = INITIAL_STATE, action) => {
-  switch (action) {
-  case GET_POKEMONS:
-    return state;
+  switch (action.type) {
+  case SET_POKEDEX:
+    return { ...state, pokedex: action.payload };
+  case SET_POKEMONS:
+    return { ...state, pokemons: action.payload };
+  case SET_IS_LOADING:
+    return { ...state, isLoading: action.payload };
+  case SET_IS_ERROR:
+    return { ...state, isError: action.payload };
   default:
     return state;
   }
