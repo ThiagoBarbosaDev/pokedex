1776692d4382d096aff473106af0e475dcf8bbcd
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-02-13 01:27:52 -0300

    chore(header): linting

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 function Header() {
-  const name = useSelector((state) => state.detailsReducer?.details?.name);
   return (
     <header className={ styles.container }>
       <Link to="/" className={ styles.title }>POKEDEX</Link>
