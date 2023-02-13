b2c3587d032980fe68e9963dd21d2fb5e8d38565
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-02-13 01:11:40 -0300

    feat(details): added fetching logic and data formattation

--- src/redux/actions/getDetails.js
+++ src/redux/actions/getDetails.js
@@ -15,12 +15,26 @@ const setDetails = (payload) => ({
   payload,
 });
 
+const transformDetails = (speciesData, pokemonData) => ({
+  name: speciesData.name,
+  genus: speciesData.genera[7].genus,
+  height: pokemonData.height,
+  weight: pokemonData.weight,
+  flavorText: speciesData.flavor_text_entries[0].flavor_text,
+  number: pokemonData.id,
+  pictureUrl: pokemonData.sprites.versions['generation-i']['red-blue'].front_default,
+});
+
 const getDetails = (name) => async (dispatch) => {
   try {
     dispatch(setIsLoading(true));
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    const data = await response.json();
-    dispatch(setDetails(data));
+    const speciesResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`);
+    const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const speciesData = await speciesResponse.json();
+    const pokemonData = await pokemonResponse.json();
+    const data = await Promise.all([speciesData, pokemonData]);
+    const entryData = transformDetails(data[0], data[1]);
+    dispatch(setDetails(entryData));
   } catch (error) {
     setIsError(error);
   } finally {
