4c551bc3addd62d4ca1e17c39993e884d1903714
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-02-13 01:23:34 -0300

    refactor(header): added back to home link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 function Header() {
   const name = useSelector((state) => state.detailsReducer?.details?.name);
   return (
     <header className={ styles.container }>
-      <h1 className={ styles.title }>{ name?.toUpperCase() || 'POKEDEX' }</h1>
+      <Link to="/" className={ styles.title }>POKEDEX</Link>
       <div className={ styles.pokeball } />
     </header>
   );
