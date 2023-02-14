80dc2fd50b2db725083e4793012dfcbd1f4805a0
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-02-14 03:28:24 -0300

    feat: filter styles, footer styles and links, general tweaks on spacings

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { FaSearch } from 'react-icons/fa';
 import { setFilterName } from '../../redux/actions/filterActions';
+import styles from './SearchBar.module.scss';
 
 function SearchBar() {
   const [search, setSearch] = useState('');
   const dispatch = useDispatch();
 
   return (
-    <section>
+    <section className={ styles.container }>
       <input
+        className={ styles.search }
         type="text"
         value={ search }
         onChange={ (event) => setSearch(event.target.value) }
         placeholder="Search By Name"
       />
-      <button onClick={ () => dispatch(setFilterName(search)) }>
-        Search
+      <button
+        onClick={ () => dispatch(setFilterName(search)) }
+        className={ styles['search-button'] }
+      >
+        <FaSearch className={ styles['search-icon'] } />
       </button>
     </section>
   );
